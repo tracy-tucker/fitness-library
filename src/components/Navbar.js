d3b37622c60aad78ed112644e02dd3fea154dc61
ttucker <tracytuckeremail@gmail.com>
2021-11-30 12:45:12 -0600

    Adding React Icons to project

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,29 @@
 import React from "react"
 import { Link } from "gatsby"
+import { FiAlignJustify } from "react-icons/fi"
 
 const Navbar = () => {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-          <li>
-            <Link to="/exercises">Exercises</Link>
-          </li>
-          <li>
-            <Link to="/tags">Tags</Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <nav>
+      <FiAlignJustify />
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+        <li>
+          <Link to="/exercises">Exercises</Link>
+        </li>
+        <li>
+          <Link to="/tags">Tags</Link>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
