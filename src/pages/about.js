fb0ac808503e89ebc1e1067d4d04445e9c58f385
ttucker <tracytuckeremail@gmail.com>
2021-11-29 20:47:09 -0600

    Still testing styling files

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,13 @@ import React from "react"
 import Layout from "../components/Layout"
 
 const about = () => {
-  return <Layout>hello from about page</Layout>
+  return (
+    <Layout>
+      <div className="about-heading">
+        <h1>hello from about page</h1>
+      </div>
+    </Layout>
+  )
 }
 
 export default about
