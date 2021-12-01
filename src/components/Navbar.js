11ea150f1550ff04d2591ada28f02a608e1931c2
ttucker <tracytuckeremail@gmail.com>
2021-11-30 19:10:06 -0600

    Organizing Navbar with styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,82 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
 import { FiAlignJustify } from "react-icons/fi"
+import logo from "../assets/images/FunFitness.svg"
 
 const Navbar = () => {
+  const [show, setShow] = useState(false)
   return (
-    <nav>
-      <FiAlignJustify />
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-        <li>
-          <Link to="/exercises">Exercises</Link>
-        </li>
-        <li>
-          <Link to="/tags">Tags</Link>
-        </li>
-      </ul>
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <Link to="/">
+            <img src={logo} alt="FunFitness logo" />
+          </Link>
+          <button className="nav-btn" onClick={() => setShow(!show)}>
+            <FiAlignJustify />
+          </button>
+        </div>
+        <div className={show ? "nav-links show-links" : "nav-links"}>
+          <Link
+            to="/"
+            className="nav-link"
+            activeClassName="active-link"
+            onClick={() => setShow(false)}
+          >
+            home
+          </Link>
+          <Link
+            to="/exercises"
+            className="nav-link"
+            activeClassName="active-link"
+            onClick={() => setShow(false)}
+          >
+            exercises
+          </Link>
+          <Link
+            to="/about"
+            className="nav-link"
+            activeClassName="active-link"
+            onClick={() => setShow(false)}
+          >
+            about
+          </Link>
+          <Link
+            to="/tags"
+            className="nav-link"
+            activeClassName="active-link"
+            onClick={() => setShow(false)}
+          >
+            tags
+          </Link>
+          <div className="nav-link contact-link">
+            <Link to="/contact" className="btn" onClick={() => setShow(false)}>
+              contact
+            </Link>
+          </div>
+        </div>
+      </div>
     </nav>
+    // <nav>
+    //   <FiAlignJustify />
+    //   <ul>
+    //     <li>
+    //       <Link to="/">Home</Link>
+    //     </li>
+    //     <li>
+    //       <Link to="/about">About</Link>
+    //     </li>
+    //     <li>
+    //       <Link to="/contact">Contact</Link>
+    //     </li>
+    //     <li>
+    //       <Link to="/exercises">Exercises</Link>
+    //     </li>
+    //     <li>
+    //       <Link to="/tags">Tags</Link>
+    //     </li>
+    //   </ul>
+    // </nav>
   )
 }
 
