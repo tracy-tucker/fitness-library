130adbf5713a88adc4ef427f40dd0d2b40beb5eb
ttucker <tracytuckeremail@gmail.com>
2021-12-01 12:12:50 -0600

    Adding content to About page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,39 @@
 import React from "react"
 import Layout from "../components/Layout"
+import { StaticImage } from "gatsby-plugin-image"
+import { Link } from "gatsby"
 
 const About = () => {
   return (
     <Layout>
       <div>
-        <h1>hello from about page</h1>
+        <h1>About FunFitness</h1>
+        <main className="page">
+          <section className="about-page">
+            <article>
+              <h2>PBR&B XOXO mlkshk, listicle actually.</h2>
+              <p>
+                Palo santo cardigan post-ironic austin pabst, asymmetrical
+                franzen hot chicken la croix photo booth. Deep v unicorn cray,
+                tattooed celiac put a bird on it 90's asymmetrical.
+              </p>
+              <p>
+                Palo santo cardigan post-ironic austin pabst, asymmetrical
+                franzen hot chicken la croix photo booth. Deep v unicorn cray,
+                tattooed celiac put a bird on it 90's asymmetrical.
+              </p>
+              <Link to="/contact" className="btn">
+                Contact Us
+              </Link>
+            </article>
+            <StaticImage
+              src="../assets/images/about.jpg"
+              alt="Person leaping"
+              className="about-img"
+              placeholder="blurred"
+            />
+          </section>
+        </main>
       </div>
     </Layout>
   )
