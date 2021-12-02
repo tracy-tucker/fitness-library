e17a877916e804072d1f22c51ed97ae812620075
ttucker <tracytuckeremail@gmail.com>
2021-12-02 12:48:24 -0600

    Image styling to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,27 @@
 import React from "react"
 import Layout from "../components/Layout"
+import { StaticImage } from "gatsby-plugin-image"
 
 export default function Home() {
   return (
     <Layout>
+      <main className="page">
+        <header className="hero">
+          <StaticImage
+            src="../assets/images/main.jpg"
+            alt="Fitness image"
+            className="hero-img"
+            placeholder="tracedSVG"
+            layout="fullWidth"
+          ></StaticImage>
+          <div className="hero-container">
+            <div className="hero-text">
+              <h1>Fun Fitness</h1>
+              <h4>Making fitness... Fun!</h4>
+            </div>
+          </div>
+        </header>
+      </main>
       <h1>Hello page!</h1>
     </Layout>
   )
