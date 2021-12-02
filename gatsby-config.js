bf6602f3fc9162db3e7beebdb22e24a5dc05e87d
ttucker <tracytuckeremail@gmail.com>
2021-12-02 13:12:59 -0600

    Adding siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,9 +3,30 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
+/** Site Metadata */
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: "Fun Fitness",
+    description: "Simple fitness instruction",
+    author: "Tracy Tucker",
+    person: {
+      name: "jane",
+      age: 32,
+      simpleData: ["item 1", "item 2"],
+      complexData: [
+        {
+          name: "john",
+          age: 38,
+        },
+        {
+          name: "jane",
+          age: 32,
+        },
+      ],
+    },
+  },
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-image`,
