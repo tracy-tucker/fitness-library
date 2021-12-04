dafe4ab52185d4a4ca90ff21d0a12ab2e13acbbf
ttucker <tracytuckeremail@gmail.com>
2021-12-04 07:25:23 -0600

    Adding images file source in gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,5 +32,12 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
   ],
 }
