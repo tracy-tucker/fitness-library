5230ee075e017557e4d0163563631fd88191de16
ttucker <tracytuckeremail@gmail.com>
2021-12-08 13:31:03 -0600

    Rendering exercixses from Contentful

--- src/components/AllExercises.js
+++ src/components/AllExercises.js
@@ -1,8 +1,30 @@
 import React from "react"
 import ExercisesList from "./ExercisesList"
 import TagsList from "./TagsList"
+import { graphql, useStaticQuery } from "gatsby"
+
+const query = graphql`
+  {
+    allContentfulExercise(sort: { order: ASC, fields: title }) {
+      nodes {
+        id
+        title
+        sets
+        repetitions
+        content {
+          tags
+        }
+        image {
+          gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG)
+        }
+      }
+    }
+  }
+`
 
 const AllExercises = () => {
+  const data = useStaticQuery(query)
+  console.log(data)
   return (
     <div>
       <h4>All Exercises</h4>
