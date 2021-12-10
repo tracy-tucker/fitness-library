346c89a1b4ad2a407570053d06f0886004750956
ttucker <tracytuckeremail@gmail.com>
2021-12-10 16:53:28 -0600

    Using utilities function to feed tags onto home page

--- src/components/TagsList.js
+++ src/components/TagsList.js
@@ -1,9 +1,25 @@
 import React from "react"
 import setupTags from "../utils/setupTags"
+import { Link } from "gatsby"
 
 const TagsList = ({ exercises }) => {
   const newTags = setupTags(exercises)
-  return <div className="tag-container">this is tagslist</div>
+  return (
+    <div className="tag-container">
+      <h4>Exercises</h4>
+      <div className="tags-list">
+        {newTags.map((tag, index) => {
+          // deconstructuring
+          const [text, value] = tag
+          return (
+            <Link to={`/${text}`} key={index}>
+              {text} ({value})
+            </Link>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default TagsList
