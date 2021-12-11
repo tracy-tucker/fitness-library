35ff0866808bb9e9e891473bb85ea2c24e06618a
ttucker <tracytuckeremail@gmail.com>
2021-12-11 06:59:47 -0600

    Feeding in tags to the Tags page

--- src/pages/tags.js
+++ src/pages/tags.js
@@ -1,8 +1,40 @@
 import React from "react"
 import Layout from "../components/Layout"
+import { graphql, Link } from "gatsby"
+import setupTags from "../utils/setupTags"
 
-const Tags = () => {
-  return <Layout>tags page</Layout>
+const Tags = ({ data }) => {
+  const newTags = setupTags(data.allContentfulExercise.nodes)
+
+  return (
+    <Layout>
+      <main className="page">
+        <section className="tags-page">
+          {newTags.map((tag, index) => {
+            const [text, value] = tag
+            return (
+              <Link to={`/${text}`} key={index} className="tag">
+                <h5>{text}</h5>
+                <p>{value} exercise</p>
+              </Link>
+            )
+          })}
+        </section>
+      </main>
+    </Layout>
+  )
 }
+
+export const query = graphql`
+  {
+    allContentfulExercise {
+      nodes {
+        content {
+          tags
+        }
+      }
+    }
+  }
+`
 
 export default Tags
