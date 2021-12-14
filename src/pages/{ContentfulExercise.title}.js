3adb48c26089cd7c7aa898ca2f0f730733bbe618
ttucker <tracytuckeremail@gmail.com>
2021-12-13 18:19:30 -0600

    Adding exercise content from DB

--- src/pages/{ContentfulExercise.title}.js
+++ src/pages/{ContentfulExercise.title}.js
@@ -1,13 +1,73 @@
 import React from "react"
+import { graphql, Link } from "gatsby"
+import Layout from "../components/Layout"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import { BsClockHistory, BsClock, BsPeople } from "react-icons/bs"
 
-const ExerciseTemplate = props => {
-  console.log(props)
+const ExerciseTemplate = ({ data }) => {
+  const {
+    title,
+    executionTime,
+    content,
+    repetitions,
+    sets,
+    description: { description },
+    image,
+  } = data.contentfulExercise
+  const pathToImage = getImage(image)
+  const { tags, instructions, setup, equipment } = content
   return (
-    <div>
-      <h4>Exercise Template</h4>
-      <h5>{props.params.title}</h5>
-    </div>
+    <Layout>
+      <main className="page">
+        <div className="exercise-page">
+          {/* hero */}
+          <GatsbyImage image={pathToImage} alt={title} className="about-img" />
+          <article className="exercise-info">
+            <h2>{title}</h2>
+            <p>{description}</p>
+            <div className="exercise-icons">//</div>
+            {/* tags */}
+            <p className="exercise-tags">
+              Tags :
+              {tags.map((tag, index) => {
+                return (
+                  <Link to={`/${tag}`} key={index}>
+                    {tag}
+                  </Link>
+                )
+              })}
+            </p>
+          </article>
+          <section className="exercise-hero"></section>
+          {/* rest of content */}
+          <section className="exercise-content"></section>
+        </div>
+      </main>
+    </Layout>
   )
 }
+
+export const query = graphql`
+  query getSingleExercise($title: String) {
+    contentfulExercise(title: { eq: $title }) {
+      title
+      executionTime
+      content {
+        setup
+        instructions
+        tags
+        equipment
+      }
+      description {
+        description
+      }
+      repetitions
+      sets
+      image {
+        gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG)
+      }
+    }
+  }
+`
 
 export default ExerciseTemplate
