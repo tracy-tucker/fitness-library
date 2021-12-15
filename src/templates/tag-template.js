cb5d95a37338ef144dced534c1471e9e74e6bfaa
ttucker <tracytuckeremail@gmail.com>
2021-12-15 13:06:58 -0600

    Creating dynamic pages for tags using NodeJS

--- src/templates/tag-template.js
+++ src/templates/tag-template.js
@@ -1,11 +1,39 @@
 import React from "react"
+import { graphql } from "gatsby"
+import ExercisesList from "../components/ExercisesList"
+import Layout from "../components/Layout"
 
-const TagTemplate = () => {
+const TagTemplate = ({ data, pageContext }) => {
+  const exercises = data.allContentfulExercise.nodes
   return (
-    <div>
-      <h2>Tag Template Page</h2>
-    </div>
+    <Layout>
+      <main className="page">
+        <h2>{pageContext.tag}</h2>
+        <div className="exercises">
+          <ExercisesList exercises={exercises} />
+        </div>
+      </main>
+    </Layout>
   )
 }
+
+export const query = graphql`
+  query GetExerciseByTag($tag: String) {
+    allContentfulExercise(
+      sort: { fields: title, order: ASC }
+      filter: { content: { tags: { eq: $tag } } }
+    ) {
+      nodes {
+        title
+        id
+        executionTime
+        repetitions
+        image {
+          gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG)
+        }
+      }
+    }
+  }
+`
 
 export default TagTemplate
