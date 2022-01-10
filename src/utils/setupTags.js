2d69f7d66ae7337de498e8dd15f3aa5fed0c16a9
Tracy Tucker <tracytuckeremail@gmail.com>
2022-01-10 17:51:36 -0600

    adding projects notes

--- src/utils/setupTags.js
+++ src/utils/setupTags.js
@@ -3,6 +3,7 @@ const setupTags = exercises => {
 
   exercises.forEach(exercise => {
     exercise.content.tags.forEach(tag => {
+      console.log('olg tags', tag)
       if (allTags[tag]) {
         allTags[tag] = allTags[tag] + 1
       } else {
@@ -11,12 +12,23 @@ const setupTags = exercises => {
     })
   })
   const newTags = Object.entries(allTags).sort((a, b) => {
+    // console.log(tag)
     const [firstTag] = a
     const [secondTag] = b
     return firstTag.localeCompare(secondTag)
   })
-  console.log(newTags)
+  
   return newTags
+  // console.log("new tags", newTags)
 }
 
 export default setupTags
+
+// Passing in exercises to the function
+// creating an empty object (allTags)
+// iterating over exercises, then iterating over tags, both with a forEach loop
+// if the allTags object has the tag property, then add tag +1 times to the object
+// if the allTags object does not have the tag property, create the property and set it equal to 1
+
+// Create a new array of tags (newTags) by using Object.entries, which returns an array of [key, value]
+// Using the sort method to then sort the resulted array
