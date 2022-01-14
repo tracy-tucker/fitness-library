412f8d44d6dded468a9bc0fe64a40abc00fa7db4
Tracy Tucker <tracytuckeremail@gmail.com>
2022-01-13 18:09:55 -0600

    CSS edits

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="page-footer">
       <p>
-        &copy; {new Date().getFullYear()} <span>FitnessLibrary</span> Built with{" "}
+        &copy; {new Date().getFullYear()} <span>Fitness Library</span> built with{" "}
         <a href="https://www.gatsbyjs.com/" target="_blank" rel="noreferrer">
           Gatsby
         </a>
